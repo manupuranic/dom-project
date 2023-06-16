6e1981feca8fd71f9eefe0dbec9471c2b059fc4e
manupuranic <manupuranic@gmail.com>
2023-06-16 15:56:30 +0530

    assignment 9 task done

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let addForm = document.getElementById("addForm");
 let itemList = document.getElementById("items");
+let filter = document.getElementById("filter");
 
 addForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
+filter.addEventListener("keyup", filterItem);
 
 function addItem(e) {
   e.preventDefault();
   let newItem = document.getElementById("item").value;
+  let desc = document.getElementById("desc").value
+    ? document.getElementById("desc").value
+    : "No Desc";
 
   // create li element
   let li = document.createElement("li");
@@ -16,6 +21,12 @@ function addItem(e) {
 
   // add the new item to the li element.
   li.appendChild(document.createTextNode(newItem));
+  let newDesc = document.createElement("p");
+  newDesc.style.display = "block";
+  newDesc.style.fontStyle = "italic";
+  newDesc.style.marginLeft = "10px";
+  newDesc.appendChild(document.createTextNode(desc));
+  li.appendChild(newDesc);
 
   // create del button
   let delbtn = document.createElement("button");
@@ -37,6 +48,7 @@ function addItem(e) {
   itemList.appendChild(li);
 
   document.getElementById("item").value = "";
+  document.getElementById("desc").value = "";
 }
 
 function removeItem(e) {
@@ -46,4 +58,22 @@ function removeItem(e) {
       itemList.removeChild(li);
     }
   }
+}
+
+function filterItem(e) {
+  let text = e.target.value.toLowerCase();
+  let items = itemList.getElementsByTagName("li");
+
+  Array.from(items).forEach((item) => {
+    let itemName = item.firstChild.textContent;
+    let detail = item.children[0].textContent;
+    if (
+      itemName.toLowerCase().indexOf(text) != -1 ||
+      (detail.trim() !== "No Desc" && detail.toLowerCase().indexOf(text) != -1)
+    ) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
